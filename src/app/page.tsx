63cebcf5b19a403668f7923dd137f934996e0bb9
Nightfall3594 <briankristoferperez28@gmail.com>
2026-01-11 18:30:46 +0800

    feat: add server status card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
+"use client";
+import ServerStatusCard from "@/components/dashboard-page/ServerStatusCard.tsx";
 
 export default function UserDashboard() {
+
+    // TODO: convert to state later
+    const isOnline = true;
+    // const [isOnline, setOnline] = useState(true);
 
     return (
         <section className="ml-60 p-30 flex flex-1 flex-col min-h-screen">
 
             {/* Server header */}
-            <div className="flex flex-row items-center justify-between gap-4 mb-10 ">
+            <div className="flex flex-row items-center justify-between gap-4 mb-6 ">
                 <div className="flex flex-col items-start gap-2">
                     <h1 className="text-4xl font-bold ">Chillingmc</h1>
                     <p className="text-md text-neutral-400 ">192.168.56.13:6543</p>
@@ -16,6 +22,9 @@ export default function UserDashboard() {
                     <button className="px-4 py-2 w-30 bg-sky-600 saturate-80  rounded-md font-bold ">Restart</button>
                 </div>
             </div>
+
+            <ServerStatusCard isOnline={isOnline}/>
+
         </section>
     )
 }
